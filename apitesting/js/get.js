bda2cc600115bf59859a613c3a42e8da3e1d4b98
abisolawal <abiso.lawal@gmail.com>
2016-05-20 18:35:11 +0100

    update get.js

--- apitesting/js/get.js
+++ apitesting/js/get.js
@@ -1,12 +1,19 @@
-$(document).ready(function(){
-  $('#testbut').click(function(){
-    var x = $('#name_var').val();
-    var y = "http://api.openweathermap.org/data/2.5/weather?q="+x+"&APPID=45e068833bdcfc8124f79fb988b69ed7";
-    $.ajax({
-      url: y,
-    }).then(function(data)
-    {
-      $('.test').append("<p>" + data.weather[0].main + "</p>");
-    });
-  });
-});
+<html>
+<head>
+  <title>My REST Api test</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+</head>
+<body>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+  <h1>Would you like to know the weather?</h1>
+  <h2>Type in the country and find out</h2>
+
+    <input type="text" id="name_var" name="name_var"/>
+    <button id="testbut" name="testbut">Click</button>
+
+  <h2 id="test" class="test"> Checking: </h2>
+
+<script src="js/get.js"></script>
+</body>
+</html>
